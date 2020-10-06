27b69ae218c0addf6e8f30adc1650bbf5722a941
Rosie Choi <rosiechoimanso@gmail.com>
2020-10-06 20:40:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog 
+  
+  @@all =[]
+  
+  def initialize(dog)
+    @dog = dog
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |i|
+      puts i.dog
+    end
+  end
+  
+  def self.clear_all
+end
+
+  
+  
+end
